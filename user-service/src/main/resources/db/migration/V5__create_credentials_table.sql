f7488d3f253a8d168cf04d6bfcd9f5446055e28c
Juan Sebastian Caviedez Fernandez <1005945503@u.icesi.edu.co>
2025-11-03 19:02:00 -0500

    refactor: update SQL migration files for users, address, credentials, and verification tokens; standardize timestamp defaults and column order

--- user-service/src/main/resources/db/migration/V5__create_credentials_table.sql
+++ user-service/src/main/resources/db/migration/V5__create_credentials_table.sql
@@ -1,15 +1,12 @@
-
 CREATE TABLE credentials (
 	credential_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_id INT(11),
-	username VARCHAR(255),
-	password VARCHAR(255),
-	role VARCHAR(255),
-	is_enabled BOOLEAN DEFAULT false,
-	is_account_non_expired BOOLEAN DEFAULT true,
-	is_account_non_locked BOOLEAN DEFAULT true,
-	is_credentials_non_expired BOOLEAN DEFAULT true,
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP
+	username VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	role_id VARCHAR(255) NOT NULL,
+	is_enabled BOOLEAN DEFAULT TRUE,
+	is_account_non_expired BOOLEAN DEFAULT TRUE,
+	is_account_non_locked BOOLEAN DEFAULT TRUE,
+	is_credentials_non_expired BOOLEAN DEFAULT TRUE,
+	user_id INT(11)
 );
 
